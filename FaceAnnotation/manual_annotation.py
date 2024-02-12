b1b0374d832310bf2ee1849ca96627fa6d9ce28a
nakaba7 <yukinakaba@gmail.com>
2024-02-13 02:54:56 +0900

    1

--- FaceAnnotation/manual_annotation.py
+++ FaceAnnotation/manual_annotation.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
 import os
+import argparse
 
 """
-Video2jpg.pyで切り出した画像に対して、手動で顔の特徴点をアノテーションするサンプルコード。
-アノテーションする枚数はmanual_img_numで指定する。
+顔画像に対して、手動で顔の特徴点をアノテーションするサンプルコード。
+コマンドライン引数でフォルダ名とアノテーションする画像の枚数を指定する。
 """
 
 def manual_annotation(input_path, output_img_path, output_csv_path):
@@ -47,22 +48,27 @@ def manual_annotation(input_path, output_img_path, output_csv_path):
     # Press any key to close the image window and print the selected landmarks coordinates.
     # Uncomment the last two lines to save the coordinates to a text file.
 
-if __name__ == '__main__':
-    manual_img_num = 10
-    folder_name = 'Nakabayashi_raspicamera'
-
-    input_frames_path = os.listdir(f'FrameImages/{folder_name}')
-    output_img_dir = f'FrameImages/{folder_name}_Annotated/Images'
-    output_csv_dir = f'FrameImages/{folder_name}_Annotated/CSVs'
+def main(args):
+    folder_name = args.f
+    manual_img_num = args.n
+    input_Imagess_path = os.listdir(f'FaceData/{folder_name}')
+    output_img_dir = f'FaceData/{folder_name}_Annotated/Images'
+    output_csv_dir = f'FaceData/{folder_name}_Annotated/CSVs'
     os.makedirs(output_img_dir, exist_ok=True)
     os.makedirs(output_csv_dir, exist_ok=True)
     img_count = 0
-    for img in input_frames_path:
+    for img in input_Imagess_path:
         if img_count == manual_img_num:
             print('Manual annotation finished')
             break
-        input_img = os.path.join(f'FrameImages/{folder_name}', img)
+        input_img = os.path.join(f'FaceData/{folder_name}', img)
         manual_annotation(input_img, os.path.join(output_img_dir, f'{img[:-4]}_annotated.jpg'), os.path.join(output_csv_dir, f'{img[:-4]}_annotated.csv'))
         img_count += 1
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', default='hoge', type=str,  help='folder name of the images to be annotated')
+    parser.add_argument('-n', default=10, type=int,  help='number of images to be annotated')
+    main(parser.parse_args())
 
 
