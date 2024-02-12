7084e09f4b85cbbed4661e2a08431141bce9f22f
nakaba7 <yukinakaba@gmail.com>
2024-02-12 17:17:57 +0900

    1

--- CameraCalibration/CameraCalibration.py
+++ CameraCalibration/CameraCalibration.py
@@ -2,59 +2,61 @@
 # -*- coding: utf-8 -*-
 
 import cv2
-import matplotlib.pyplot as plt
+import os
 import numpy as np
 import glob
 
+square_size = 2.5      # 正方形の1辺のサイズ[cm]
+pattern_size = (6, 8)  # 交差ポイントの数
+#folder_name = "ChessBoard_a"
+folder_name = "ChessBoard_b"
+reference_img = 61 # 参照画像の枚数
 
-square_size = 3.6      # 正方形の1辺のサイズ[cm]
-pattern_size = (6, 9)  # 交差ポイントの数
+# 新しいフォルダーの名前
+output_folder_name = "Processed_Images"
+# folder_nameの下に新しいフォルダーを作成（存在しない場合）
+output_path = os.path.join(folder_name, output_folder_name)
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
 
-reference_img = 40 # 参照画像の枚数
-
-pattern_points = np.zeros( (np.prod(pattern_size), 3), np.float32 ) #チェスボード（X,Y,Z）座標の指定 (Z=0)
-pattern_points[:,:2] = np.indices(pattern_size).T.reshape(-1, 2)
+pattern_points = np.zeros((np.prod(pattern_size), 3), np.float32)  # チェスボード（X,Y,Z）座標の指定 (Z=0)
+pattern_points[:, :2] = np.indices(pattern_size).T.reshape(-1, 2)
 pattern_points *= square_size
 objpoints = []
 imgpoints = []
 
-#capture = cv2.VideoCapture(0)
-
-images = glob.glob('ImagesForCalibration/*.png')
-count=0
+images = glob.glob(f'{folder_name}/*.jpg')
+count = 0
 for filepath in images:
-# 画像の取得
     img = cv2.imread(filepath)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Find the chess board corners
-    # If desired number of corners are found in the image then ret = true
-    ret, corner = cv2.findChessboardCorners(gray, pattern_size)
-    # コーナーがあれば
-    if ret == True:
-        print("detected coner!")
-        print(str(len(objpoints)+1) + "/" + str(reference_img))
+    ret, corners = cv2.findChessboardCorners(gray, pattern_size)
+    if ret:
+        print("detected corner!")
+        print(f"{len(objpoints)+1}/{reference_img}")
         term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1)
-        cv2.cornerSubPix(gray, corner, (5,5), (-1,-1), term)
-        imgpoints.append(corner.reshape(-1, 2))   #appendメソッド：リストの最後に因数のオブジェクトを追加
+        cv2.cornerSubPix(gray, corners, (5, 5), (-1, -1), term)
+        imgpoints.append(corners.reshape(-1, 2))
         objpoints.append(pattern_points)
-    if count>=reference_img-1:
-        cv2.imshow('image', img)
+
+        # コーナーを描画
+        cv2.drawChessboardCorners(img, pattern_size, corners, ret)
+        # 画像を新しいフォルダーに保存
+        img_filename = os.path.basename(filepath)
+        cv2.imwrite(os.path.join(output_path, img_filename), img)
+        
+    if count >= reference_img - 1:
         break
-    count+=1
-    """
-    cv2.imshow('image', img)
-    # 毎回判定するから 200 ms 待つ．遅延するのはココ
-    if cv2.waitKey(200) & 0xFF == ord('q'):
-        break
-    """
+    count += 1
 
 print("calculating camera parameter...")
 # 内部パラメータを計算
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 # 計算結果を保存
-np.save("mtx", mtx) # カメラ行列
-np.save("dist", dist.ravel()) # 歪みパラメータ
+np.save(os.path.join(f"{folder_name}_mtx"), mtx)  # カメラ行列
+np.save(os.path.join(f"{folder_name}_dist"), dist.ravel())  # 歪みパラメータ
 # 計算結果を表示
 print("RMS = ", ret)
 print("mtx = \n", mtx)
