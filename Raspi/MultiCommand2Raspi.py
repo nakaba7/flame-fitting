8edc0f8b472b785949ef4174e4ff73c539dccd2d
nakaba7 <yukinakaba@gmail.com>
2024-04-15 15:35:18 +0900

    1

--- Raspi/MultiCommand2Raspi.py
+++ Raspi/MultiCommand2Raspi.py
@@ -7,8 +7,8 @@ Wi-fi経由で行う
 """
 
 # Raspberry Piのホスト名またはIPアドレスとポート番号
-HOST_A = "192.168.11.57"  # Raspberry Piのホスト名またはIPアドレスを設定
-HOST_B = "192.168.11.56"
+HOST_A = "192.168.100.34"  #Raspberry Pi 5(両目カメラ)
+HOST_B = "192.168.100.37"  #Raspberry Pi 3(口周辺カメラ)
 PORT = 46361              # Raspberry Pi側のスクリプトで使用しているポート番号
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sa:
@@ -26,6 +26,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sa:
 
             # Enterが押されたらRaspberry Piにシグナルを送信
             print(datetime.datetime.now())
+            
             sa.sendall(b'c')
             sb.sendall(b'c')
             print("Signal sent to Raspberry Pi to capture an image.")
